0b753529fbe48cc23ef5f925a77d545af08fd33f
LuciusLan <cifellan@hotmail.com>
2020-10-28 22:17:45 +0800

    Added scope trainer

--- trainer.py
+++ trainer.py
@@ -9,6 +9,7 @@ from tqdm import tqdm
 from sklearn.metrics import classification_report
 
 from util import pack_subword_pred
+from params import param
 
 DEVICE = torch.device('cuda')
 
@@ -286,5 +287,146 @@ class CueTrainer(object):
             # early_stopping
             if self.early_stopping:
                 self.early_stopping.epoch_step(current=logs['val_cue_f1']+logs['val_cuesep_f1'])
+                if self.early_stopping.stop_training:
+                    break
+
+class ScopeTrainer(object):
+    def __init__(self, model, n_gpu, logger, criterion, optimizer, lr_scheduler,
+                 label2id, gradient_accumulation_steps, grad_clip=0.0,early_stopping=None,
+                 resume_path=None, training_monitor=None, model_checkpoint=None):
+
+        self.n_gpu = n_gpu
+        self.model = model
+        self.logger = logger
+        self.criterion = criterion
+        self.optimizer = optimizer
+        self.label2id = label2id
+        self.grad_clip = grad_clip
+        self.lr_scheduler = lr_scheduler
+        self.early_stopping = early_stopping
+        self.model_checkpoint = model_checkpoint
+        self.training_monitor = training_monitor
+        self.gradient_accumulation_steps = gradient_accumulation_steps
+
+        # self.model, self.device = model_device(n_gpu=self.n_gpu, model=self.model)
+        self.device = DEVICE
+        #self.id2label = {y: x for x, y in label2id.items()}
+        self.start_epoch = 1
+        self.global_step = 0
+        if resume_path:
+            self.logger.info(f"\nLoading checkpoint: {resume_path}")
+            resume_dict = torch.load(resume_path + '\\checkpoint_info.bin')
+            best = resume_dict['epoch']
+            self.start_epoch = resume_dict['epoch']
+            if self.model_checkpoint:
+                self.model_checkpoint.best = best
+            self.logger.info(f"\nCheckpoint '{resume_path}' and epoch {self.start_epoch} loaded")
+
+    def save_info(self, epoch, best):
+        model_save = self.model.module if hasattr(self.model, 'module') else self.model
+        state = {"model": model_save,
+                 'epoch': epoch,
+                 'best': best}
+        return state
+
+    def valid_epoch(self, data_features, is_bert):
+        pbar = tqdm(total=len(data_features), desc='Evaluating')
+        valid_loss = AverageMeter()
+        wrap_scope_pred = []
+        wrap_scope_tar = []
+        for step, f in enumerate(data_features):
+            input_lens = f[4]
+            num_labels = param.label_dim
+            input_ids = f[0].to(self.device)
+            padding_mask = f[1].to(self.device)
+            subword_mask = f[-1].to(self.device)
+            scopes = f[2].to(self.device)
+
+            self.model.eval()
+            with torch.no_grad():
+                scope_logits = self.model(input_ids, padding_mask)[0]
+                active_padding_mask = padding_mask.view(-1) == 1
+                loss = self.criterion(scope_logits.view(-1, num_labels)[active_padding_mask], scopes.view(-1)[active_padding_mask])
+            valid_loss.update(val=loss.item(), n=input_ids.size(0))
+            if is_bert:
+                scope_pred, scope_tar = pack_subword_pred(scope_logits.detach().cpu(), scopes.detach().cpu(), subword_mask.detach().cpu())
+                
+            else:
+                scope_pred = scope_logits.argmax()
+                scope_tar = scopes
+
+            for i1, sent in enumerate(scope_pred):
+                for i2, _ in enumerate(sent):
+                    wrap_scope_pred.append(scope_pred[i1][i2])
+                    wrap_scope_tar.append(scope_tar[i1][i2])
+
+            pbar.update()
+            pbar.set_postfix({'loss': loss.item()})
+        self.logger.info('\n Token Level F1:\n')
+        scope_val_info = classification_report(wrap_scope_tar, wrap_scope_pred, output_dict=True, digits=5)
+        if 'cuda' in str(self.device):
+            torch.cuda.empty_cache()
+        return scope_val_info
+
+    def train_epoch(self, data_loader, valid_data):
+        pbar = tqdm(total=len(data_loader), desc='Training')
+        num_labels = param.label_dim
+        tr_loss = AverageMeter()
+        for step, batch in enumerate(data_loader):
+            self.model.train()
+            batch = tuple(t.to(self.device) for t in batch)
+            input_ids, padding_mask, scopes, input_len, subword_mask = batch
+            scope_logits = self.model(input_ids, padding_mask)[0]
+            active_padding_mask = padding_mask.view(-1) == 1
+            loss = self.criterion(scope_logits.view(-1, num_labels)[active_padding_mask], scopes.view(-1)[active_padding_mask])
+
+            #if len(self.n_gpu.split(",")) >= 2:
+            #    loss = loss.mean()
+            
+            # scale the loss
+            if self.gradient_accumulation_steps > 1:
+                loss = loss / self.gradient_accumulation_steps
+
+            loss.backward()
+            # gradient clip
+            clip_grad_norm_(self.model.parameters(), self.grad_clip)
+            if (step + 1) % self.gradient_accumulation_steps == 0:
+                self.optimizer.step()
+                self.optimizer.zero_grad()
+                self.global_step += 1
+            tr_loss.update(loss.item(), n=1)
+            pbar.update()
+            pbar.set_postfix({'loss': loss.item()})
+        info = {'loss': tr_loss.avg}
+        if "cuda" in str(self.device):
+            torch.cuda.empty_cache()
+        return info
+
+    def train(self, train_data, valid_data, epochs, is_bert=False):
+        #seed_everything(seed)
+        for epoch in range(self.start_epoch, self.start_epoch + int(epochs)):
+            self.logger.info(f"Epoch {epoch}/{int(epochs)}")
+            train_log = self.train_epoch(train_data, valid_data)
+            scope_log = self.valid_epoch(valid_data, is_bert)
+
+            scope_f1 = target_weight_score(scope_log, ['0', '1', '2'])
+            logs = {'loss': train_log['loss'], 'val_scope_token_f1': scope_f1[0]}
+            #logs = dict(train_log, **cue_log['weighted avg'], **cue_sep_log['weighted avg'])
+            #show_info = f'Epoch: {epoch} - ' + "-".join([f' {key}: {value:.4f} ' for key, value in logs.items()])
+            #self.logger.info(show_info)
+            #self.logger.info("The entity scores of valid data : ")
+            '''for key, value in class_info.items():
+                info = f'Entity: {key} - ' + "-".join([f' {key_}: {value_:.4f} ' for key_, value_ in value.items()])
+                self.logger.info(info)'''
+
+            if hasattr(self.lr_scheduler,'epoch_step'):
+                self.lr_scheduler.epoch_step(metrics=scope_f1[0], epoch=epoch)
+            # save log
+            if self.training_monitor:
+                self.training_monitor.epoch_step(logs)
+
+            # early_stopping
+            if self.early_stopping:
+                self.early_stopping.epoch_step(current=logs['val_scope_token_f1'])
                 if self.early_stopping.stop_training:
                     break
